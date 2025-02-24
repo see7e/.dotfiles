9e5f7f416deef3737ed00b47dfe26543f8d78aa4
Gabryel Nóbrega <gabryelster@gmail.com>
2025-02-24 13:38:16 +0000

    [updt] Add NVChad configurations and add tmux plugin folder

---
+++ res/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,48 @@
+require("lazy").setup({{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
+ }})
+
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
+
+  { "wakatime/vim-wakatime", lazy = false },
+
+  git = {
+    timeout = 1200, -- increase timeout to 1200 seconds (20 minutes)
+  },
+}
+
